86b9038a84233c5ea189473b6f02de0d545413ba
Pepito-Manaloto <aaronjohn.asuncion@gmail.com>
2014-10-24 13:39:50 +0800

    Changed DATE to DATETIME, CURDATE() to NOW(), order by id to last_updated.

--- my_vocabulary.sql
+++ my_vocabulary.sql
@@ -13,8 +13,8 @@ CREATE TABLE `vocabulary`
    `foreign_id` TINYINT UNSIGNED NOT NULL,
    `english_word` VARCHAR(70) CHARACTER SET utf8mb4 NOT NULL,
    `foreign_word` VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL,
-   `datein` DATE NOT NULL,
-   `last_updated` DATE NOT NULL,
+   `datein` DATETIME NOT NULL,
+   `last_updated` DATETIME NOT NULL,
    PRIMARY KEY(`id`),
    KEY `english_word_index`(`english_word`, `id`),
    KEY `foreign_word_index`(`foreign_word`),
@@ -26,7 +26,7 @@ DELIMITER $$
 CREATE PROCEDURE `Add_Vocabulary`(IN `pEnglish_word` VARCHAR(70), IN `pForeign_word` VARCHAR(100) CHARSET utf8mb4, IN `pForeign_language` TINYINT)
 BEGIN
 
-  INSERT INTO vocabulary(english_word, foreign_word, foreign_id, datein, last_updated) VALUES(pEnglish_word, pForeign_word, pForeign_language, CURDATE(), CURDATE());
+  INSERT INTO vocabulary(english_word, foreign_word, foreign_id, datein, last_updated) VALUES(pEnglish_word, pForeign_word, pForeign_language, NOW(), NOW());
 
 END $$
 DELIMITER;
@@ -49,7 +49,7 @@ BEGIN
       SELECT english_word, foreign_word FROM vocabulary v, foreign_language f WHERE v.foreign_id = f.id AND f.language = pForeign_language ORDER BY english_word ASC;
     ELSEIF (letter LIKE 'Rec') THEN
       SELECT COUNT(*) FROM vocabulary v, foreign_language f WHERE v.foreign_id = f.id AND f.language = pForeign_language INTO total;
-      SELECT english_word, foreign_word FROM vocabulary v, foreign_language f WHERE v.foreign_id = f.id AND f.language = pForeign_language ORDER BY v.id DESC;
+      SELECT english_word, foreign_word FROM vocabulary v, foreign_language f WHERE v.foreign_id = f.id AND f.language = pForeign_language ORDER BY v.last_updated DESC;
     ELSE
       SELECT COUNT(*) FROM vocabulary v, foreign_language f WHERE v.foreign_id = f.id AND f.language = pForeign_language AND english_word LIKE CONCAT(letter, '%') INTO total;
       SELECT english_word, foreign_word FROM vocabulary v, foreign_language f WHERE v.foreign_id = f.id AND f.language = pForeign_language AND english_word LIKE CONCAT(letter, '%');
@@ -64,9 +64,9 @@ BEGIN
 
   CASE word_language
     WHEN 0 THEN
-      UPDATE vocabulary INNER JOIN foreign_language ON foreign_language.id = vocabulary.foreign_id SET english_word = pEnglish_word, last_updated = CURDATE() WHERE foreign_word = pForeign_word AND language = pForeign_language;
+      UPDATE vocabulary INNER JOIN foreign_language ON foreign_language.id = vocabulary.foreign_id SET english_word = pEnglish_word, last_updated = NOW() WHERE foreign_word = pForeign_word AND language = pForeign_language;
     WHEN 1 THEN
-      UPDATE vocabulary INNER JOIN foreign_language ON foreign_language.id = vocabulary.foreign_id SET foreign_word = pForeign_word, last_updated = CURDATE() WHERE english_word = pEnglish_word AND language = pForeign_language;
+      UPDATE vocabulary INNER JOIN foreign_language ON foreign_language.id = vocabulary.foreign_id SET foreign_word = pForeign_word, last_updated = NOW() WHERE english_word = pEnglish_word AND language = pForeign_language;
     ELSE
       /* DO NOTHING */SELECT 1;
     END CASE;
@@ -75,9 +75,10 @@ END $$
 DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE `Get_Vocabularies`(IN pEnglish_word VARCHAR(70), IN pForeign_word VARCHAR(100) CHARSET utf8mb4, IN pForeign_language VARCHAR(30), IN word_language TINYINT)
+CREATE PROCEDURE `Get_Vocabularies`(IN pLast_updated DATETIME, OUT recently_added_count INT)
 BEGIN
 
+    SELECT COUNT(*) FROM vocabulary WHERE last_updated > pLast_updated INTO recently_added_count;
     SELECT english_word, foreign_word FROM vocabulary v, foreign_language f WHERE v.foreign_id = f.id AND f.language = "Hokkien"  ORDER BY english_word ASC;
     SELECT english_word, foreign_word FROM vocabulary v, foreign_language f WHERE v.foreign_id = f.id AND f.language = "Japanese"  ORDER BY english_word ASC;
     SELECT english_word, foreign_word FROM vocabulary v, foreign_language f WHERE v.foreign_id = f.id AND f.language = "Mandarin"  ORDER BY english_word ASC;
